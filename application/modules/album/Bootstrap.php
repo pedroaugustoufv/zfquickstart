ec67f49988187ea3a2c1f5f93fc6e8b81f7a125d
Pedro Augusto <pedro.augusto.ufv@gmail.com>
2014-05-06 10:54:44 -0300

    criacao do modulo album

---
+++ application/modules/album/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+class Album_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+	protected $moduleName="Album";
+	
+	public function __construct($application)
+	{
+		parent::__construct($application);
+	
+		$this->modulePath = APPLICATION_PATH.'/modules/'.strtolower($this->moduleName).'/';
+	
+	}
+}
+
