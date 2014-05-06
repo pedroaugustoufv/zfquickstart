ef8d82c1bb96e35e90b2cf522e8360a8907d7248
Pedro Augusto <pedro@webferoz.com.br>
2014-05-05 23:31:47 -0300

    layout diferente por modulo

--- application/modules/guestbook/Bootstrap.php
+++ application/modules/guestbook/Bootstrap.php
@@ -4,8 +4,8 @@ class Guestbook_Bootstrap extends Zend_Application_Module_Bootstrap
 {
 	protected $moduleName="Guestbook";
 	
-	public function __construct($application) {
-	
+	public function __construct($application)
+	{
 		parent::__construct($application);
 	
 		$this->modulePath = APPLICATION_PATH.'/modules/'.strtolower($this->moduleName).'/';
